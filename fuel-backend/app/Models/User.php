e69a8b6c7c37f195e87519c7210324916e82192b
Yogaaprtma <111202113472@mhs.dinus.ac.id>
2026-03-25 11:30:17 +0700

    update: Model User

--- fuel-backend/app/Models/User.php
+++ fuel-backend/app/Models/User.php
@@ -3,11 +3,9 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 
