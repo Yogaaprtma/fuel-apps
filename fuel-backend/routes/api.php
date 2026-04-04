68093584df33b2effbad3cc78791d62e54667937
Yogaaprtma <yogapratama010503@gmail.com>
2026-04-04 13:11:35 +0700

    update file fuel-backend/routes/api.php

--- fuel-backend/routes/api.php
+++ fuel-backend/routes/api.php
@@ -12,7 +12,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/auth/login', [AuthController::class, 'login']);
 Route::get('/track', [DeliveryController::class, 'publicTrack']);
 
-Route::middleware('auth::sanctum')->group(function () {
+Route::middleware('auth:sanctum')->group(function () {
     // Auth
     Route::get('/auth/me', [AuthController::class, 'me']);
     Route::post('/auth/logout', [AuthController::class, 'logout']);
