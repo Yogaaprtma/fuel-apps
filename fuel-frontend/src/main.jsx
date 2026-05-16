789f380f30c551b883ab68cf58b6cb697c893950
Yogaaprtma <yogapratama010503@gmail.com>
2026-05-16 18:47:06 +0900

    memperbaiki file fuel-frontend/src/main.jsx

--- fuel-frontend/src/main.jsx
+++ fuel-frontend/src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js')
+    .then(reg => console.log('SW Registered!', reg))
+    .catch(err => console.log('SW Error', err));
+}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
